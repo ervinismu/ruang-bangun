72fee58d1546b38f2fb0ae8e877839182498ca0d
blacksyan <ilham.adhy@gmail.com>
2018-11-08 15:32:48 +0700

    Add comment

--- bola.rb
+++ bola.rb
@@ -3,11 +3,13 @@ class Bola
     @@pi = 3.14
     def initialize(r)
         # set rumus volume bola
+        # r adalah jari-jari
         @r = r
         
     end
 
     def volume_bola
+        # rumus volume adalah phi * r * r *r
         return @@pi * @r * @r *@r
     end
 
